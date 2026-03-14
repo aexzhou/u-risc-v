492a14d4a2b302d7d2d5be90cb455c1b558d1b0e
Alex Zhou <alexzhou330@gmail.com>
2026-03-13 23:41:16 -0700

    #20, #21 [RTL] Assigned 2b11 aluop to Load and Store instructions

--- hdl/cpu/datapath/rv_datapath_ctrl.sv
+++ hdl/cpu/datapath/rv_datapath_ctrl.sv
@@ -13,8 +13,8 @@ always_comb begin
     case (opcode)
         7'b0010011: {alusrc, memtoreg, regwrite, memread, memwrite, branch, alu_op} = 8'b10100000; // I-type
         7'b0110011: {alusrc, memtoreg, regwrite, memread, memwrite, branch, alu_op} = 8'b00100010; // R-type
-        7'b0000011: {alusrc, memtoreg, regwrite, memread, memwrite, branch, alu_op} = 8'b11110000; // I-type: LOAD instructions
-        7'b0100011: {alusrc, memtoreg, regwrite, memread, memwrite, branch, alu_op} = 8'b1x001000; // S-type
+        7'b0000011: {alusrc, memtoreg, regwrite, memread, memwrite, branch, alu_op} = 8'b11110011; // I-type: LOAD instructions
+        7'b0100011: {alusrc, memtoreg, regwrite, memread, memwrite, branch, alu_op} = 8'b1x001011; // S-type
         7'b1100011: {alusrc, memtoreg, regwrite, memread, memwrite, branch, alu_op} = 8'b0x000101; // B-type
         default:    {alusrc, memtoreg, regwrite, memread, memwrite, branch, alu_op} = 8'd0;
     endcase
